b6d927c66464bba963582929086fb395fc777bd5
MartinDrost <info@martindrost.nl>
2015-11-02 20:23:44 +0100

    Minor changes

--- FFMpeg/src/ambilight/domain/ScreenCapture.java
+++ FFMpeg/src/ambilight/domain/ScreenCapture.java
@@ -1,65 +1,61 @@
-package ambilight.domain;
+    package ambilight.domain;
 
-import java.awt.Dimension;
-import java.awt.Rectangle;
-import java.awt.Robot;
-import java.awt.Toolkit;
-import java.awt.image.BufferedImage;
-import org.bytedeco.javacv.*;
-import org.bytedeco.javacpp.*;
+    import java.awt.Dimension;
+    import java.awt.Toolkit;
+    import java.awt.image.BufferedImage;
+    import org.bytedeco.javacv.FFmpegFrameGrabber;
+    import org.bytedeco.javacv.Java2DFrameConverter;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    /*
+     * To change this license header, choose License Headers in Project Properties.
+     * To change this template file, choose Tools | Templates
+     * and open the template in the editor.
+     */
 
-/**
- *
- * @author Martin
- */
-public class ScreenCapture {
-    //frameGrabber
-    FFmpegFrameGrabber frameGrabber;
-    BufferedImage bufferedImage;
-    org.bytedeco.javacv.Java2DFrameConverter frameConverter;
-    //Variable to store the screen dimensions
-    private Dimension screenSize;
-    
-    public ScreenCapture()
-    {
-        try
+    /**
+     *
+     * @author Martin
+     */
+    public class ScreenCapture {
+        //frameGrabber
+        FFmpegFrameGrabber frameGrabber;
+        BufferedImage bufferedImage;
+        Java2DFrameConverter frameConverter;
+        private Dimension screenSize;
+
+        public ScreenCapture()
         {
-            this.screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-            frameConverter = new Java2DFrameConverter();
+            try
+            {
+                this.screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+                frameConverter = new Java2DFrameConverter();
+                bufferedImage = null;
+
+                this.frameGrabber = new FFmpegFrameGrabber(":0.0+0,0");
+                this.frameGrabber.setFrameRate(30);
+                this.frameGrabber.setFormat("x11grab");
+                this.frameGrabber.setImageWidth(screenSize.width);
+                this.frameGrabber.setImageHeight(screenSize.height);
+                this.frameGrabber.start();
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+        public BufferedImage grabScreen()
+        {
             bufferedImage = null;
-            
-            this.frameGrabber = new FFmpegFrameGrabber(":0.0+0,0");
-            this.frameGrabber.setFrameRate(30);
-            this.frameGrabber.setFormat("x11grab");
-            this.frameGrabber.setImageWidth(screenSize.width);
-            this.frameGrabber.setImageHeight(screenSize.height);
-            this.frameGrabber.start();
-        }
-        catch(Exception e)
-        {
-            e.printStackTrace();
+            try
+            {
+                bufferedImage = frameConverter.convert(this.frameGrabber.grabFrame(true, false, false));
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+
+            return bufferedImage;
         }
     }
-    
-    public BufferedImage grabScreen()
-    {
-        bufferedImage = null;
-        try
-        {
-            bufferedImage = frameConverter.convert(this.frameGrabber.grab());
-        }
-        catch(Exception e)
-        {
-            e.printStackTrace();
-        }
-        
-        return bufferedImage;
-    }
-    
-}
