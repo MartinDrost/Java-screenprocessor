15f270e8c44a16eb13f2277bdbac4ccba3ccbad4
MFDemon <info@martindrost.nl>
2015-11-04 19:07:40 +0100

    Added tutorials and cleaned up code

--- FFMpeg/src/ambilight/domain/ScreenCapture.java
+++ FFMpeg/src/ambilight/domain/ScreenCapture.java
@@ -1,61 +1,115 @@
-    package ambilight.domain;
+package ambilight.domain;
 
-    import java.awt.Dimension;
-    import java.awt.Toolkit;
-    import java.awt.image.BufferedImage;
-    import org.bytedeco.javacv.FFmpegFrameGrabber;
-    import org.bytedeco.javacv.Java2DFrameConverter;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.bytedeco.javacv.FFmpegFrameGrabber;
+import org.bytedeco.javacv.FrameGrabber;
+import org.bytedeco.javacv.Java2DFrameConverter;
 
-    /*
-     * To change this license header, choose License Headers in Project Properties.
-     * To change this template file, choose Tools | Templates
-     * and open the template in the editor.
-     */
-
+/**
+ *
+ * @author Martin
+ */
+public class ScreenCapture {
     /**
-     *
-     * @author Martin
+     * Eerst maken we een field binnen de klasse voor de FFmpegFrameGrabber.
+     * Omdat je waarschijnlijk maar 1 instantie van de klasse gaat gebruiken 
+     * kan je hem het beste hier cachen en eenmaal instantiëren in de constructor
      */
-    public class ScreenCapture {
-        //frameGrabber
-        FFmpegFrameGrabber frameGrabber;
-        BufferedImage bufferedImage;
-        Java2DFrameConverter frameConverter;
-        private Dimension screenSize;
+    FFmpegFrameGrabber frameGrabber;
+    
+    /**
+     * De framegrabber geeft een instantie van de Frame klasse van JavaCV terug.
+     * Om deze om te kunnen zetten naar een BufferedImage kunnen we gebruik maken
+     * van de Java2DFrameConverter.
+     */
+    Java2DFrameConverter frameConverter;
+    
+    /**
+     * In dit attribuut gaan we de schermopname opslaan. 
+     */
+    private BufferedImage image;
+    
+    
+    /**
+     * In de constructor vullen we de hierboven gedefinieerde attributen.
+     */
+    public ScreenCapture()
+    {
+        try {
+            
+            this.frameConverter = new Java2DFrameConverter();
+            this.image = null;
 
-        public ScreenCapture()
-        {
-            try
-            {
-                this.screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-                frameConverter = new Java2DFrameConverter();
-                bufferedImage = null;
-
-                this.frameGrabber = new FFmpegFrameGrabber(":0.0+0,0");
-                this.frameGrabber.setFrameRate(30);
-                this.frameGrabber.setFormat("x11grab");
-                this.frameGrabber.setImageWidth(screenSize.width);
-                this.frameGrabber.setImageHeight(screenSize.height);
-                this.frameGrabber.start();
-            }
-            catch(Exception e)
-            {
-                e.printStackTrace();
-            }
-        }
-
-        public BufferedImage grabScreen()
-        {
-            bufferedImage = null;
-            try
-            {
-                bufferedImage = frameConverter.convert(this.frameGrabber.grabFrame(true, false, false));
-            }
-            catch(Exception e)
-            {
-                e.printStackTrace();
-            }
-
-            return bufferedImage;
+            /**
+             * Hier definieren we de instellingen voor de frameGrabber.
+             */
+            
+            /**
+             * de FFmpegFrameGrabber krijgt als parameters een string met ":0.0+0,0".
+             * Dit doen we om aan te geven dat we het 1e aangesloten beeldscherm willen
+             * gebruiken als input apparaat. Hier kunnen andere strings worden 
+             * ingevoerd om bijvoorbeeld een ander beeldscherm te selecteren, 
+             * een media apparaat of een videobestand.
+             * Bij setFormat we het formaat van de videodrivers. 
+             * 
+             * Deze instellingen zijn voor voor elk OS
+             * anders, hieronder een lijstje met de verschillende parameters:
+             * Linux: 
+                * FFmpegFrameGrabber(":0.0+0,0")
+                * setFormat("x11grab")
+             * 
+             * Windows: 
+                * FFmpegFrameGrabber("desktop")
+                * setFormat("gdigrab")
+             * als deze niet werkt:
+                * FFmpegFrameGrabber("video=screen-capture-recorder")
+                * setFormat("dshow")
+             * 
+             */
+            this.frameGrabber = new FFmpegFrameGrabber("desktop");
+            this.frameGrabber.setFormat("gdigrab");
+            
+            /**
+             * Hier stellen we de breedte en de hoogte van de schermopname in.
+             */
+            this.frameGrabber.setImageWidth(Toolkit.getDefaultToolkit().getScreenSize().width);
+            this.frameGrabber.setImageHeight(Toolkit.getDefaultToolkit().getScreenSize().height);
+            
+            /**
+             * En vervolgens starten we de schermopname.
+             */
+            this.frameGrabber.start();
+        } catch (FrameGrabber.Exception ex) {
+            Logger.getLogger(ScreenCapture.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+    
+    /**
+     * De grabScreen() methode maakt een schermopname van het huidige beeld op 
+     * scherm. Dit beeld wordt teruggegeven in een BufferedImage, het gebruik
+     * van dit object wordt later beschreven in de tutorial.
+     * @return 
+     */
+    public BufferedImage grabScreen()
+    {
+        try
+        {
+            /**
+             * Hier roepen we de grabFrame methode van de frameGrabber aan en zetten
+             * deze om naar een BufferedImage m.b.v. de frameConverter.convert() methode.
+             */ 
+            image = frameConverter.convert(this.frameGrabber.grabFrame(true, false, false));
+        }
+        catch(Exception e)
+        {
+            image = null;
+            e.printStackTrace();
+        }
+        
+        return image;
+    }
+    
+}
--- Robots/src/ambilight/domain/ScreenCapture.java
+++ Robots/src/ambilight/domain/ScreenCapture.java
@@ -1,12 +1,10 @@
 package ambilight.domain;
 
-import java.awt.Dimension;
+import java.awt.AWTException;
 import java.awt.Rectangle;
 import java.awt.Robot;
 import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
-import org.bytedeco.javacv.*;
-import org.bytedeco.javacpp.*;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -19,41 +17,70 @@ import org.bytedeco.javacpp.*;
  * @author Martin
  */
 public class ScreenCapture {
-    //frameGrabber
-    Robot robot;
-    BufferedImage bufferedImage;
+    /**
+     * Eerst maken we een field binnen de klasse voor de Robot.
+     * Omdat je waarschijnlijk maar 1 instantie van de klasse gaat gebruiken 
+     * kan je hem het beste hier cachen en eenmaal instantiëren in de constructor
+     */
+    private Robot robot;
     
-    //Variable to store the screen dimensions
-    private Dimension screenSize;
+    /**
+     * Hier gaan we de hoogte en de breedte van ons scherm in opslaan.
+     * We gaan ook dit attribuut vullen in de constructor.
+     */
+    private Rectangle screenSize;
     
+    /**
+     * In dit attribuut gaan we de schermopname opslaan. 
+     */
+    private BufferedImage image;
+    
+    
+    /**
+     * In de constructor vullen we de hierboven gedefinieerde attributen.
+     */
     public ScreenCapture()
     {
         try
         {
-            this.screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-            robot = new Robot();
-            bufferedImage = null;
+            this.robot = new Robot();
+            this.screenSize = new Rectangle(0, 0, 
+                    Toolkit.getDefaultToolkit().getScreenSize().width,
+                    Toolkit.getDefaultToolkit().getScreenSize().height);
+            this.image = null;
         }
-        catch(Exception e)
+        catch(AWTException e)
         {
             e.printStackTrace();
         }
     }
     
+    /**
+     * De grabScreen() methode maakt een schermopname van het huidige beeld op 
+     * scherm. Dit beeld wordt teruggegeven in een BufferedImage, het gebruik
+     * van dit object wordt later beschreven in de tutorial.
+     * @return 
+     */
     public BufferedImage grabScreen()
     {
-        bufferedImage = null;
         try
         {
-            Rectangle area = new Rectangle(0, 0, this.screenSize.width, this.screenSize.height);
-            bufferedImage = robot.createScreenCapture(area);
+            /**
+             * Hier roepen we de .createScreenCapture() methode aan van de Robot
+             * klasse. Deze methode verwacht een Rectangle object met de coordinaten,
+             * breedte en hoogte van het gebied waar je een opname van wilt maken.
+             * Deze rectangle hebben we al gemaakt en gevuld in de constructor en 
+             * hebben we screenSize genoemd.
+             */ 
+            image = robot.createScreenCapture(this.screenSize);
         }
         catch(Exception e)
         {
+            image = null;
             e.printStackTrace();
         }
         
-        return bufferedImage;
+        return image;
     }
     
 }
